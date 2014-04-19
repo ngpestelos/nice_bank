39a5880275ad80b4599decb66181eb186076b6fd
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2014-04-19 12:41:17 +0800

    Initial commit

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,16 @@
+class Account
+  def initialize(amount)
+  end
+end
+
+Given /^I have deposited \$(\d+) in my account$/ do |amount|
+  Account.new(amount.to_i)
+end
+
+When(/^I request \$(\d+)$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^\$(\d+) should be dispensed$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
