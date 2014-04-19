6488f439d39ac92db36b6c72140c37a3417b3766
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2014-04-19 13:27:33 +0800

    extract my_account to a helper method

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -14,20 +14,28 @@ class Teller
   end
 end
 
+module KnowsMyAccount
+  def my_account
+    @my_account ||= Account.new
+  end
+end
+
+World(KnowsMyAccount)
+
 CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
   digits.to_i
 end
 
 Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  @my_account = Account.new
-  @my_account.deposit(amount)
-  expect(@my_account.balance).to equal(amount.to_i),
-    "Expected the balance to be #{amount} but it was #{@my_account.balance}"
+  my_account = Account.new
+  my_account.deposit(amount)
+  expect(my_account.balance).to equal(amount.to_i),
+    "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
 When(/^I withdraw (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
   teller = Teller.new
-  teller.withdraw_from(@my_account, amount)
+  teller.withdraw_from(my_account, amount)
 end
 
 Then(/^\$(\d+) should be dispensed$/) do |arg1|
