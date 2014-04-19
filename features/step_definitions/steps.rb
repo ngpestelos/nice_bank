43c226111e8eedcb1e6f791065a84c0e5763742d
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2014-04-19 13:23:51 +0800

    add a Teller to withdraw money from

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -8,19 +8,26 @@ class Account
   end
 end
 
+class Teller
+  def withdraw_from(account, amount)
+
+  end
+end
+
 CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
   digits.to_i
 end
 
 Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  my_account = Account.new
-  my_account.deposit(amount)
-  expect(my_account.balance).to equal(amount.to_i),
-    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  @my_account = Account.new
+  @my_account.deposit(amount)
+  expect(@my_account.balance).to equal(amount.to_i),
+    "Expected the balance to be #{amount} but it was #{@my_account.balance}"
 end
 
-When(/^I request \$(\d+)$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I withdraw (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
+  teller = Teller.new
+  teller.withdraw_from(@my_account, amount)
 end
 
 Then(/^\$(\d+) should be dispensed$/) do |arg1|
